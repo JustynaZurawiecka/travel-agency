d63025399aa422a8974427cc477a302ece0b58c8
Justyna Żurawiecka <justyna2106@wp.pl>
2020-11-07 16:07:26 +0100

    Add filters for trips

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -8,16 +8,20 @@ class TripListOptions extends React.Component {
   handleTags(tag, checked){
     if(checked) {
       console.log('Adding tag', tag);
-      // TODO - use action dispatcher from props
+      this.props.addTag(tag);
     } else {
       console.log('Removing tag', tag);
+      this.props.removeTag(tag);
       // TODO - use action dispatcher from props
     }
   }
 
   handleDuration(type, value){
     console.log('Changing duration', type, value);
-    // TODO - use action dispatcher from props
+    this.props.changeSearchDuration({
+      name: type,
+      value: value,
+    });
   }
 
   handleSearch(phrase){
@@ -73,6 +77,9 @@ TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  changeSearchDuration: PropTypes.func,
+  addTag: PropTypes.func,
+  removeTag: PropTypes.func,
 };
 
 export default TripListOptions;
