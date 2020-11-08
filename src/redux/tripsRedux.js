2f59df65e32c93d96ed101e2138b60f728aaee4b
Justyna Żurawiecka <justyna2106@wp.pl>
2020-11-08 21:43:54 +0100

    Submodul 17.5 correct filter mechanism

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -29,19 +29,12 @@ export const getFilteredTrips = ({trips, filters}) => {
 export const getTripById = ({trips}, tripId) => {
   const filtered = trips.filter((trip) => trip.id == tripId);
 
-  filtered.filter((trip) => trip.id == tripId);
-  // TODO - filter trips by tripId
-
   console.log('filtering trips by tripId:', tripId, filtered);
   return filtered.length ? filtered[0] : {error: true};
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
-
-  // TODO - filter trips by countryCode
-
-  filtered.filter((trip) => trip.country.code == countryCode);
+  const filtered = trips.filter((trip) => trip.country.code == countryCode);
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
   return filtered.length ? filtered : [{error: true}];
